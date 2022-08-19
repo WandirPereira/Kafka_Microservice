e02e9888f60530c7b5833f93b797d0a9ae37b7d3
Wandir Pereira Filho <wandir@gmail.com>
2022-08-18 21:42:40 -0300

    Inicializandoo o repositório

---
+++ str-producer/src/test/java/com/github/wandirpereira/strproducer/StrProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.wandirpereira.strproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
